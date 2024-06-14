8c0a4bf1ab75d6dd364a5b31e5427a7c00aa33aa
Alondra Chable Carrillo <106031585+AloloCarr@users.noreply.github.com>
2024-06-13 23:42:06 -0600

    actualizar y eliminar usuarios listos

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,73 +1,107 @@
-const sql = require('mssql');
+const sql = require("mssql");
+const bcrypt = require("bcryptjs");
 
 //traer todos usuarios
-const getUsers = async (req, res ) =>{
-    try {
-        const pool = await sql.connect();
-        const result = await pool.request().query('SELECT * FROM Users');
-        res.json(result.recordset);
-    } catch (err) {
-        res.status(500).send(err.message);
-    }
-}
+const getUsers = async (req, res) => {
+  try {
+    const pool = await sql.connect();
+    const result = await pool.request().query("SELECT * FROM Users");
+    res.json(result.recordset);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+};
 
 //traer un solo usuario
-const getUserById = async(req, res)=>{
-    try {
-        const pool = await sql.connect();
-        const result = await pool.request()
-        .input('id', sql.Int, req.params.id)
-        .query('SELECT * FROM Users WHERE id = @id');
+const getUserById = async (req, res) => {
+  try {
+    const pool = await sql.connect();
+    const result = await pool
+      .request()
+      .input("id", sql.Int, req.params.id)
+      .query("SELECT * FROM Users WHERE id = @id");
 
-        if(result.recordset.length == 0){
-            return res.status(404).send('No se encontro a ningún usuario con ese ID');
-
-        }else{
-            res.json(result.recordset[0]);
-        }
-        
-
-    } catch (err) {
-        res.status(500).send(err.message);
+    if (result.recordset.length == 0) {
+      return res.status(404).send("No se encontro a ningún usuario con ese ID");
+    } else {
+      res.json(result.recordset[0]);
     }
-}
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+};
 
 //actualizar usuario
-const updateUser = async (req, res) =>{
-    try {
-        const {name, email, password} = req.body;
-        const emailCheck = await pool.request()
-            .input('email', sql.NVarChar, email)
-            .query('SELECT * FROM Users WHERE email = @email');
+const updateUser = async (req, res) => {
+  try {
+    const pool = await sql.connect();
+    const { name, email, password } = req.body;
+    const emailCheck = await pool
+      .request()
+      .input("email", sql.NVarChar, email)
+      .query("SELECT * FROM Users WHERE email = @email");
 
-        if (emailCheck.recordset.length > 0) {
-            return res.status(400).send('El correo electrónico ya existe.');
-        }else{
-            const hashedPassword = await bcrypt.hash(password, 10);
+    const result = await pool
+      .request()
+      .input("id", sql.Int, req.params.id)
+      .query("SELECT * FROM Users WHERE id = @id");
 
-        await pool.request()
-            .input('id', sql.Int, req.params.id)
-            .input('name', sql.NVarChar, name)
-            .input('email', sql.NVarChar, email)
-            .input('password', sql.NVarChar, hashedPassword)
-            .query('UPDATE Users set name = @name, email = @email, password = @password WHERE id = @id ');
-        
-        res.status(200).send('Usuario actualizado'); 
-        }
-
-    } catch (err) {
-        res.status(500).send(err.message);
+    if (result.recordset.length == 0) {
+      return res.status(404).send("No se encontro a ningún usuario con ese ID");
     }
-}
+    if (emailCheck.recordset.length > 0) {
+      return res.status(400).send("El correo electrónico ya existe.");
+    } else {
+      const hashedPassword = await bcrypt.hash(password, 10);
+
+      await pool
+        .request()
+        .input("id", sql.Int, req.params.id)
+        .input("name", sql.NVarChar, name)
+        .input("email", sql.NVarChar, email)
+        .input("password", sql.NVarChar, hashedPassword)
+        .query(
+          "UPDATE Users set name = @name, email = @email, password = @password WHERE id = @id "
+        );
+
+      res.status(200).send("Usuario actualizado exitosamente");
+    }
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+};
 
 //borrar usuario
-const deleteUser = (req, res) =>{
+const deleteUser = async (req, res) => {
+  try {
+    const pool = await sql.connect();
+    const { name, email, password } = req.body;
 
-}
+    const result = await pool
+      .request()
+      .input("id", sql.Int, req.params.id)
+      .query("SELECT * FROM Users WHERE id = @id");
+
+    if (result.recordset.length == 0) {
+      return res.status(404).send("No se encontro a ningún usuario con ese ID");
+    } else {
+      await pool
+        .request()
+        .input("id", sql.Int, req.params.id)
+        .query(
+          "DELETE Users WHERE id = @id"
+        );
+
+      res.status(200).send("Usuario eliminado exitosamente");
+    }
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+};
 
 module.exports = {
-    getUsers,
-    getUserById,
-    updateUser,
-    deleteUser
-}
+  getUsers,
+  getUserById,
+  updateUser,
+  deleteUser,
+};
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -7,6 +7,7 @@ const userController = require('../controllers/userController');
 //RUTAS USUARIOS
 router.get('/users', userController.getUsers);
 router.get('/user/:id', userController.getUserById);
-router.put('/user/:id', );
+router.put('/user/:id', userController.updateUser);
+router.delete('/user/:id', userController.deleteUser);
 
 module.exports = router;
